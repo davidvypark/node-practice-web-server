9d2f54f2071b9402cd45179e4e41355dfbacf7aa
davidvypark <davidvypark@gmail.com>
2018-02-26 23:41:27 -0800

    added a projects page

--- server.js
+++ server.js
@@ -47,6 +47,13 @@ app.get('/about', (request, response) => {
   });
 });
 
+app.get('/projects', (request, response) => {
+  response.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    paragraph: 'These are my projects',
+  })
+})
+
 app.get('/bad', (request, response) => {
   response.send({
     errorMessage: 'Bad Request Bruh',
